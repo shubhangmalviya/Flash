bcba9b2e8946d2ecb9c9d3a53e71e852e2749b11
Shubhang <shubhcs3086@gmail.com>
2017-02-12 21:30:16 +0530

    Form Data Request Builder Under progress.

--- logic/src/main/java/com/flash/logic/rest/post/form_data/FormDataApiRequestGenerator.java
+++ logic/src/main/java/com/flash/logic/rest/post/form_data/FormDataApiRequestGenerator.java
@@ -1,9 +1,6 @@
 package com.flash.logic.rest.post.form_data;
 
-import com.squareup.javapoet.ClassName;
-import com.squareup.javapoet.ParameterizedTypeName;
-import com.squareup.javapoet.TypeName;
-import com.squareup.javapoet.TypeSpec;
+import com.squareup.javapoet.*;
 
 import javax.lang.model.element.Modifier;
 
@@ -19,14 +16,69 @@ public class FormDataApiRequestGenerator {
                 .addModifiers(Modifier.PUBLIC);
     }
 
-    public void setServiceParameters(ClassName requestClass, ClassName responseClass,
-                                     ClassName serviceClassName) {
+    public void generate(ClassName requestClass, ClassName responseClass,
+                         ClassName serviceClassName, MethodSpec servicesRequestMethod) {
+        generateSuperclassHierarchy(requestClass, responseClass, serviceClassName);
+        generateConstructor();
+        generateMakeRequestMethod(requestClass, responseClass, serviceClassName);
+        generateServiceClassMethod(responseClass, serviceClassName);
+    }
 
+    private void generateSuperclassHierarchy(ClassName requestClass, ClassName responseClass,
+                                             ClassName serviceClassName) {
         ClassName apiRequestClass = ClassName.get("com.libapi", "ApiRequest");
         TypeName superclassName = ParameterizedTypeName.get(apiRequestClass,
                 requestClass, responseClass, serviceClassName);
 
         mClassBuilder.superclass(superclassName);
+    }
 
+    private void generateServiceClassMethod(ClassName responseClass,
+                                            ClassName serviceClassName) {
+
+        ClassName clazz = ClassName.get(Class.class);
+        TypeName classOfServiceService = ParameterizedTypeName.get(clazz, serviceClassName);
+
+        MethodSpec.Builder serviceClassMethod = MethodSpec.methodBuilder("getServiceClass")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PROTECTED)
+                .returns(classOfServiceService)
+                .addStatement("return $T", responseClass);
+
+        mClassBuilder.addMethod(serviceClassMethod.build());
+    }
+
+    private void generateMakeRequestMethod(ClassName requestClass, ClassName responseClass,
+                                           ClassName serviceClassName) {
+
+        ClassName retrofitCall = ClassName.get("retrofit2", "Call");
+        TypeName callOfResponse = ParameterizedTypeName.get(retrofitCall, responseClass);
+
+        MethodSpec.Builder makeRequestMethod = MethodSpec.methodBuilder("makeRequest")
+                .addAnnotation(Override.class)
+                .addModifiers(Modifier.PROTECTED)
+                .returns(callOfResponse)
+                .addParameter(requestClass, "reqPostFormData")
+                .addParameter(serviceClassName, "service");
+        // TODO add the return parameter.
+
+        mClassBuilder.addMethod(makeRequestMethod.build());
+    }
+
+    private void generateConstructor() {
+
+        ClassName errorResponseTransformer = ClassName.get("com.libapi", "ErrorResponseTransformer");
+        ClassName serviceCreator = ClassName.get("com.libapi", "ServiceCreator");
+
+        String parameterServiceCreator = "serviceCreator";
+        String parameterErrorResponseTransformer = "errorResponseTransformer";
+
+        MethodSpec.Builder constructorBuilder = MethodSpec.constructorBuilder()
+                .addModifiers(Modifier.PUBLIC)
+                .addParameter(serviceCreator, parameterServiceCreator)
+                .addParameter(errorResponseTransformer, parameterErrorResponseTransformer)
+                .addStatement("super($N, $N);", parameterErrorResponseTransformer, parameterServiceCreator);
+
+        mClassBuilder.addMethod(constructorBuilder.build());
     }
 }
---
+++ logic/src/main/java/com/flash/logic/rest/post/form_data/FormDataMakeRequestMethodBuilder.java
@@ -0,0 +1,15 @@
+package com.flash.logic.rest.post.form_data;
+
+import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.MethodSpec;
+
+public class FormDataMakeRequestMethodBuilder {
+
+    public FormDataMakeRequestMethodBuilder(ClassName requestClass, ClassName responseClass,
+                                            ClassName serviceClassName) {
+    }
+
+    public MethodSpec generateMethod(MethodSpec servicesRequestMethod) {
+        return null;
+    }
+}
